a9e049b17a1d10186b993511f06fe30563d86bd6
bmuday <bmuday@live.fr>
2023-09-23 22:17:40 +0200

    update folder

--- next.config.js
+++ next.config.js
@@ -2,15 +2,10 @@
 const nextConfig = {
   experimental: {
     appDir: true,
-    swcPlugins: [["next-superjson-plugin", {}]]
   },
   images: {
-    domains: [
-      'res.cloudinary.com', 
-      'avatars.githubusercontent.com',
-      'lh3.googleusercontent.com'
-    ]
-  }
-}
+    domains: [],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
